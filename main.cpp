dae72615fd1e1836b5786b6b749bccfa9dc3a919
James Pope <james_pope@student.uml.edu>
2022-03-14 14:27:06 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <filesystem>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Error: no input file specified";
+        return 0;
+    }
+    return 0;
+}
