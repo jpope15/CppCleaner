2b6b87e3737f19d45ec84bf5b7a6e316aa4bde1a
James Pope <james_pope@student.uml.edu>
2022-03-14 14:50:37 -0400

    updated comments

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <filesystem>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
-    if (argc != 2)
+    if (argc != 2U)
     {
-        std::cout << "Error: no input file specified";
-        return 0;
+        std::cerr << "Error: no input file specified";
+        return 0U;
     }
-    return 0;
+
+    // opening the listed file
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Couldn't open file " << argv[1];
+        return 0U;
+    }
+
+    // check if its a valid file extension...
+
+    // parse the file...
+
+    // close input file...
+    inputFile.close();
+
+    // delete input file...
+     
+    // open input file as new output file...
+
+    // write to output file
+    return 0U;
 }
