7d439a28232711bc3499eaff25904b940d68b9c7
James Pope <james_pope@student.uml.edu>
2022-03-15 14:37:31 -0400

    Finished implementation

--- main.cpp
+++ main.cpp
@@ -15,31 +15,48 @@ int main(int argc, char* argv[])
 
     // opening the listed file
     std::ifstream inputFile(argv[1]);
+    std::ofstream outputFile("temp.txt");
+
     if (!inputFile.is_open())
     {
         std::cerr << "Couldn't open file " << argv[1] << '\n';
+
+        return 0U;
+    }
+    if (!outputFile.is_open())
+    {
+        std::cerr << "Couldn't open file temp.txt";
         return 0U;
     }
 
     // parsing the file
     std::string temp;
+    int lines_removed = 0;
     while (std::getline(inputFile, temp))
     {
-        if (boost::regex_match(temp, delim_regex))
+        if (!boost::regex_match(temp, delim_regex))
         {
-            std::cout << "found a match\n";
+            outputFile << temp << '\n';
         }
         else
         {
-            std::cout << temp << "\n";
+            lines_removed++;
         }
-    }    
+    }
     inputFile.close();
+    outputFile.close();
 
     // delete input file...
-     
-    // open input file as new output file...
+    if (std::remove(argv[1]))
+    {
+        std::cerr << "Couldn't delete file " << argv[1] << '\n';
+    }
 
-    // write to output file
+    std::rename("temp.txt", argv[1]);
+
+    // writing to stdout
+    std::cout << "CPP_CLEANER\n"
+            << "Ran on " << argv[1] << '\n'
+            << "Removed " << std::to_string(lines_removed) << " lines\n";
     return 0U;
 }
