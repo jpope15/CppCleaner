d9e28ceb7652022cdf9bfde50910b5fb5c8eb4f4
James Pope <james_pope@student.uml.edu>
2022-04-29 10:10:12 -0400

    added stub for block comments

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 #include <fstream>
 #include <boost/regex.hpp>
 
-static const boost::regex delim_regex(".*DEBUG_REMOVE.*");
+const boost::regex single_line_regex(".*\\/\\/.*DEBUG_REMOVE.*");
+const boost::regex block_comment_start_regex(".*\\/\\*.*DEBUG_REMOVE.*");
+const boost::regex block_comment_end_regex(".*\\*\\/.*");
+
+bool is_single_line_comment(const std::string& line);
+bool is_block_comment_start(const std::string& line);
+
+void handle_block_comment(const std::string& line, std::ifstream& file);
 
 int main(int argc, char* argv[])
 {
@@ -20,7 +27,6 @@ int main(int argc, char* argv[])
     if (!inputFile.is_open())
     {
         std::cerr << "Couldn't open file " << argv[1] << "\nCheck your spelling and try again\n";
-
         return 0U;
     }
     if (!outputFile.is_open())
@@ -34,23 +40,20 @@ int main(int argc, char* argv[])
     int lines_removed = 0;
     while (std::getline(inputFile, temp))
     {
-        if (!boost::regex_match(temp, delim_regex))
-        {
-            outputFile << temp << '\n';
-        }
-        else
-        {
+        if (is_single_line_comment(temp))
             lines_removed++;
-        }
+        else
+            outputFile << temp << '\n';
+
+        if (is_block_comment_start(temp))
+            handle_block_comment(temp, inputFile);
     }
     inputFile.close();
     outputFile.close();
 
     // delete input file...
     if (std::remove(argv[1]))
-    {
         std::cerr << "Couldn't delete file " << argv[1] << '\n';
-    }
 
     std::rename("temp.txt", argv[1]);
 
@@ -59,4 +62,19 @@ int main(int argc, char* argv[])
             << "Ran on " << argv[1] << '\n'
             << "Removed " << std::to_string(lines_removed) << " lines\n";
     return 0U;
+}
+
+bool is_single_line_comment(const std::string& line)
+{
+    return boost::regex_match(line, single_line_regex);
+}
+
+bool is_block_comment_start(const std::string& line)
+{
+    return boost::regex_match(line, block_comment_start_regex);
+}
+
+void handle_block_comment(const std::string& line, std::ifstream& file)
+{
+    std::cout << "\nfound block comment\n";
 }
